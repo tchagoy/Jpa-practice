5e92ca9d0d4f753ea71b3a896223c5140d65a96b
Tommy109 <arkaner89@gmail.com>
2022-10-01 18:47:49 -0600

    started pagination examples

--- src/test/java/com/tbmarketing/jpapractice/repository/CourseRepositoryTest.java
+++ src/test/java/com/tbmarketing/jpapractice/repository/CourseRepositoryTest.java
@@ -6,6 +6,8 @@ import com.tbmarketing.jpapractice.loader.InfoLoader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Collection;
 import java.util.List;
@@ -22,5 +24,32 @@ class CourseRepositoryTest {
     public void printCourses(){
         List<Course> courses = courseRepository.findAll();
         InfoLoader.printWithDelay(courses,"course",1);
+    }
+
+    /** Paging example, JPARepository already extends PagingRepository **/
+    @Test
+    public void findAllPagination(){
+        Pageable firstPageWithThreeRecords =
+                PageRequest.of(0,3); //3rd argument is a Sort object.
+
+        Pageable secondPageWithTwoRecords =
+                PageRequest.of(1,2);
+
+        List<Course> courses
+                = courseRepository.findAll(secondPageWithTwoRecords)
+                    .getContent();
+
+        Long totalElements = courseRepository.findAll(secondPageWithTwoRecords)
+                        .getTotalElements();
+
+        int totalPages = courseRepository.findAll(secondPageWithTwoRecords)
+                        .getTotalPages();
+
+        System.out.println("TOTAL ELEMENTS: " + totalElements);
+        System.out.println("TOTAL PAGES: " + totalPages);
+
+
+        InfoLoader.printWithDelay(courses, "course",3);
+
     }
 }
